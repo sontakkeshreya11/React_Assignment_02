987eb848aa55474e2726369b8a0308cb15b8e204
sontakkeshreya11 <sontakkeshreya1106@gmail.com>
2021-09-30 10:31:43 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Front from './Front';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+       <Front/>
+    </>
+  );
+}
+
+export default App;
